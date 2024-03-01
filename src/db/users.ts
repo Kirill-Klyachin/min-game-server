ad8e524b3b3cbfc5dbc5eae90781bc2663a0f480
Kirill-Klyachin <MrKiryn@yandex.ru>
2024-03-01 06:56:34 +0500

    Fixed config

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,10 +1,10 @@
 import mysql from "mysql";
 
 export const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "adidas121",
-  database: "users",
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 /* connection.connect(function (err) {
